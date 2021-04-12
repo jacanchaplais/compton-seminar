858c2e17bf855a77e4bbd1e3621ae49f1a7256fb
jc1765@york.ac.uk <jc1765@york.ac.uk>
2021-04-12 11:13:32 +0100

    removed redundant build script

--- build.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/bash
-
-pandoc --mathjax -t revealjs -s --filter pandoc-citeproc -o index.html src/slides.md \
-       -V revealjs-url=src/reveal.js \
-       -V theme=black --slide-level=2 \
-       --bibliography=refs.bib --csl=elsevier-vancouver.csl
